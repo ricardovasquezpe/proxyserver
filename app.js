be33e6103d2c65beff8d4946076bbfb4cdc32319
Ricardo Vasquez <ricardo.vasquez.pe@gmail.com>
2024-08-29 15:19:11 -0500

    fix

--- app.js
+++ app.js
@@ -9,6 +9,13 @@ app.use(express.json());
 app.post('/', async (req, res) => {
     var response = await axios.post(req.query.url, req.body);
     res.send(response.data);
-})
+});
+
+app.get('/', async (req, res) => {
+    var response = await axios.get(req.query.url, {
+        headers: req.headers
+    });
+    res.send(response.data);
+});
 
 app.listen(8080);
